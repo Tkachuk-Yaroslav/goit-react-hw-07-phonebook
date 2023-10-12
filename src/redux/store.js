4e983d53e8484e3d202ab79207ee03831e927ec7
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-10-12 14:53:17 +0300

    Commented local storage
    Commented out the code responsible for storing and reading contacts from local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,38 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import { contactSlice } from './contacts/contactsSlice';
 import { filterSlice } from './filter/filterSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['contacts'],
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   whitelist: ['contacts'],
+// };
 
 const rootReducer = combineReducers({
   contacts: contactSlice.reducer,
   filter: filterSlice.reducer,
 });
 
-const persistedReducerContact = persistReducer(persistConfig, rootReducer);
+// const persistedReducerContact = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducerContact,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: rootReducer,
+  // middleware: getDefaultMiddleware =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
